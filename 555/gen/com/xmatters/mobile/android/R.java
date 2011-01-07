d0a13ead16089e55ea6afb4e4f34a3429975e7d4
lguo <lguo@xmatters.com>
2011-01-07 14:14:55 -0800

    Creating the 555 project

--- 555/gen/com/xmatters/mobile/android/R.java
+++ 555/gen/com/xmatters/mobile/android/R.java
@@ -12,6 +12,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int splash=0x7f020001;
+        public static final int xmatters57=0x7f020002;
+    }
+    public static final class id {
+        public static final int ImageView01=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -19,5 +24,6 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int splash=0x7f040002;
     }
 }
