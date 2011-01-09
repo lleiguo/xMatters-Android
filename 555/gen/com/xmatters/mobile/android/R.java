830f656a2411bc4d7199ae11ef8df32c98c22d71
lguo <lguo@xmatters.com>
2011-01-08 17:47:00 -0800

    Signed-off-by: lguo <lguo@xmatters.com>

--- 555/gen/com/xmatters/mobile/android/R.java
+++ 555/gen/com/xmatters/mobile/android/R.java
@@ -11,19 +11,37 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int splash=0x7f020001;
-        public static final int xmatters57=0x7f020002;
+        public static final int ic_tab_artists=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int myalerts=0x7f020002;
+        public static final int myalerts_grey=0x7f020003;
+        public static final int settings=0x7f020004;
+        public static final int settings_grey=0x7f020005;
+        public static final int settings_white=0x7f020006;
+        public static final int splash=0x7f020007;
+        public static final int xmatters57=0x7f020008;
     }
     public static final class id {
-        public static final int ImageView01=0x7f050000;
+        public static final int Company=0x7f050002;
+        public static final int Password=0x7f050006;
+        public static final int Server=0x7f050000;
+        public static final int UserName=0x7f050004;
+        public static final int companyValue=0x7f050003;
+        public static final int passwordValue=0x7f050007;
+        public static final int serverValue=0x7f050001;
+        public static final int userValue=0x7f050005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int settings=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int splash=0x7f040002;
+        public static final int app_name=0x7f040000;
+        public static final int company=0x7f040004;
+        public static final int password=0x7f040006;
+        public static final int server=0x7f040003;
+        public static final int settings=0x7f040002;
+        public static final int splash=0x7f040001;
+        public static final int user=0x7f040005;
     }
 }
