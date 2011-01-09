de355b0eabc3f265975cc82ecd270e96e3134762
lguo <lguo@xmatters.com>
2011-01-09 13:21:37 -0800

    Add a placeholder in SettingActivit to handle clicking of register button

--- 555/src/com/xmatters/mobile/android/SettingsActivity.java
+++ 555/src/com/xmatters/mobile/android/SettingsActivity.java
@@ -2,13 +2,24 @@ package com.xmatters.mobile.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class SettingsActivity extends Activity  {
+public class SettingsActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        Button button = (Button)findViewById(R.id.register);
+        button.setOnClickListener(this);
     }
+
+	public void onClick(View arg0) {
+		//Call Jan's method to register
+		Log.d("SettingActivity", "registering...");
+	}
 }
